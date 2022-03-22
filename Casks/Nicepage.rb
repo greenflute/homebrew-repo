7291e729df22d65974e97f3d5eea2699e3e9214c
greenflute <greenflute@hotmail.com>
2022-03-22 14:13:40 +0100

    Create Nicepage.rb
    Added Nicepage.app

---
+++ Casks/Nicepage.rb
@@ -0,0 +1,18 @@
+cask "Nicepage" do
+  version "4.7.1"
+  sha256 "aee9534eb0fecca35f60b59d50078ff618dc634d3c40cb4a5716fb06faca603d"
+
+  url "https://get.nicepage.com/Nicepage-#{version}.dmg"
+  name "Nicepage"
+  desc "Web Site Builder Software"
+  homepage "https://www.nicepage.com/"
+
+  livecheck do
+    url "https://nicepage.com/get/mac"
+    strategy :header_match
+    regex(/Nicepage-(\d+(?:\.\d+)+)\.dmg/i)
+  end
+
+  app "Nicepage.app"
+end
+
