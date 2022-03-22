61a7cfa6c66c611a8ce1790f0d10e4dc61444cc7
greenflute <greenflute@hotmail.com>
2022-03-22 14:23:30 +0100

    fix Name mismatch

--- Casks/nicepage.rb
+++ Casks/nicepage.rb
@@ -1,9 +1,9 @@
-cask "Nicepage" do
+cask "nicepage" do
   version "4.7.1"
   sha256 "aee9534eb0fecca35f60b59d50078ff618dc634d3c40cb4a5716fb06faca603d"
 
   url "https://get.nicepage.com/Nicepage-#{version}.dmg"
-  name "Nicepage"
+  name "nicepage"
   desc "Web Site Builder Software"
   homepage "https://www.nicepage.com/"
 
