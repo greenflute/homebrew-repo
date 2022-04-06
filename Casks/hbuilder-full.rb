4ee6095e5a2d8dc07d8ca2d03a6c3ffa5b6007db
greenflute <greenflute@hotmail.com>
2022-04-06 02:14:43 +0200

    Create hbuilder-full.rb

---
+++ Casks/hbuilder-full.rb
@@ -0,0 +1,19 @@
+cask "hbuilderx-full" do
+  version "3.3.13.20220314"
+  sha256 "291e37b2ea283937472044ced78e9a29414ffd79e308d7c7c12fbd1cd684c819"
+
+  url "https://download1.dcloud.net.cn/download/HBuilderX.#{version}.full.dmg",
+      verified: "download1.dcloud.net.cn/"
+  name "HBuilderX-Full"
+  desc "HTML editor"
+  homepage "https://www.dcloud.io/hbuilderx.html"
+
+  livecheck do
+    url "https://download1.dcloud.net.cn/hbuilderx/release.json"
+    strategy :page_match do |page|
+      JSON.parse(page)["version"]
+    end
+  end
+
+  app "HBuilderX.app"
+end
