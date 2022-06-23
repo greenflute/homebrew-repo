0793811383dbdb8fdb33a8217bb5b93a3117a268
greenflute <greenflute@hotmail.com>
2022-06-24 00:12:13 +0200

    央视影音客户端桌面版

---
+++ Casks/cctv-client.rb
@@ -0,0 +1,11 @@
+cask "cctv-client" do
+  version "2.1.0.0_1001"
+
+  url "https://download.cntv.cn/cbox/macv2/ysyy_v#{version}_setup.dmg"
+  name "央视影音客户端桌面版"
+  desc "央视影音客户端桌面版"
+  homepage "https://app.cctv.com"
+
+  app "央视影音.app"
+end
+
