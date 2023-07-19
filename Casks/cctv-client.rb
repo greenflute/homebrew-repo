1349a5985d9b4b12e392ac974bb6f42def4419de
greenflute <greenflute@hotmail.com>
2023-07-19 12:26:53 +0200

    Update cctv-client.rb

--- Casks/cctv-client.rb
+++ Casks/cctv-client.rb
@@ -1,7 +1,7 @@
 cask "cctv-client" do
   version "2.2.0.0_1001"
 
-  url "https://download.cntv.cn/cbox/macv2/ysyy_v#{version}_setup.dmg"
+  url "https://download.cntv.cn/cbox/macv2/ysyy_v#{version}_setup.dmg.zip"
   name "央视影音客户端桌面版"
   desc "央视影音客户端桌面版"
   homepage "https://app.cctv.com"
