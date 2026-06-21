3003452597c695a900f144b7a10523a194384d4d
greenflute <greenflute@hotmail.com>
2026-06-21 10:14:11 +0200

    fix syntax

--- Casks/synergy@1.rb
+++ Casks/synergy@1.rb
@@ -1,11 +1,10 @@
-cask "synergy" do
+cask "synergy@1" do
   version "v3.0.73.7-rc2"
 
-  url "https://rc.symless.com/synergy3/#{version}/synergy-macOS_x64-#{version}.dmg
+  url "https://rc.symless.com/synergy3/#{version}/synergy-macOS_x64-#{version}.dmg"
   name "synergy"
   desc "Share one mouse and keyboard across multiple computers"
   homepage "https://symless.com/synergy"
 
   app "Synergy.app"
 end
-
